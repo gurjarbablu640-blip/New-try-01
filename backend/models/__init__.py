60b4b0e9af0f1c07b272cc3106eac5a7a6aa929d
gurjarbablu640-blip <gurjarbablu640@gmail.com>
2026-08-14 14:55:47 +0530

    fix: register knowledge models in active ORM registry

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -6,34 +6,18 @@ from models.intent_signal import CompanyIntentSignal
 from models.outreach_draft import OutreachDraft
 from models.activity import CRMActivity
 from models.order import Order
-from models.pipeline import (
-    PipelineStage,
-    PipelineActivity,
-    ABTestResult,
-    LeadRating,
-)
-from models.sales_os import (
-    Opportunity,
-    SalesTask,
-    SalesNote,
-    Quotation,
-    QuotationItem,
-    Instrument,
-    InstrumentAlias,
-    PriceHistory,
-    AIFeedback,
-    LearningRule,
-)
+from models.pipeline import PipelineStage, PipelineActivity, ABTestResult, LeadRating
+from models.sales_os import Opportunity, SalesTask, SalesNote, Quotation, QuotationItem, Instrument, InstrumentAlias, PriceHistory, AIFeedback, LearningRule
 from models.web_research import WebResearchItem
 from models.campaign import Campaign, CampaignStep, CampaignRecipient, CampaignEvent
 from models.competitor_intel import CompetitorProfile, CompetitorObservation
+from models.knowledge import KnowledgeDocument, KnowledgeChunk
 
 __all__ = [
-    "Company", "Person", "CompanyWebsiteIntel", "CompanyIntentSignal",
-    "OutreachDraft", "PipelineStage", "PipelineActivity", "ABTestResult",
-    "LeadRating", "Order", "CRMActivity", "Opportunity", "SalesTask",
-    "SalesNote", "Quotation", "QuotationItem", "Instrument", "InstrumentAlias",
-    "PriceHistory", "AIFeedback", "LearningRule", "WebResearchItem",
-    "Campaign", "CampaignStep", "CampaignRecipient", "CampaignEvent",
-    "CompetitorProfile", "CompetitorObservation",
+    "Company", "Person", "CompanyWebsiteIntel", "CompanyIntentSignal", "OutreachDraft",
+    "PipelineStage", "PipelineActivity", "ABTestResult", "LeadRating", "Order", "CRMActivity",
+    "Opportunity", "SalesTask", "SalesNote", "Quotation", "QuotationItem", "Instrument",
+    "InstrumentAlias", "PriceHistory", "AIFeedback", "LearningRule", "WebResearchItem",
+    "Campaign", "CampaignStep", "CampaignRecipient", "CampaignEvent", "CompetitorProfile",
+    "CompetitorObservation", "KnowledgeDocument", "KnowledgeChunk",
 ]
